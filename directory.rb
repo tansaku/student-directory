03722bad950ba837e9d89777736bcc2775d3113b
Sam Joseph <tansaku@gmail.com>
2013-12-24 10:19:51 +0000

    cleaning a little

--- directory.rb
+++ directory.rb
@@ -1,3 +1,5 @@
+student_count = 9
+# first, we print the list of students
 puts "The students of my cohort at Makers Academy"
 puts "-------------"
 puts "Dr. Hannibal Lecter"
@@ -9,3 +11,5 @@ puts "The Alien"
 puts "Terminator"
 puts "Freddy Kruger"
 puts "The Joker"
+# finally, we print the total
+puts "Overall, we have #{student_count} great students"
