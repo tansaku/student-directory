d232aafbbc2df902de5575556face0a10e52c0ed
Sam Joseph <tansaku@gmail.com>
2013-12-24 10:27:15 +0000

    interactive

--- directory.rb
+++ directory.rb
@@ -27,7 +27,27 @@ def print_footer(names)
   puts "Overall, we have #{names.length} great students"
 end
 
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # create an empty array
+  students = []
+  # get the first name
+  name = gets.chomp
+  # while the name is not empty, repeat this code
+  while !name.empty? do
+    # add the student hash to the array
+    students << {:name => name, :cohort => :november}
+    puts "Now we have #{students.length} students"
+    # get another name from the user
+    name = gets.chomp
+  end
+  # return the array of students
+  students
+end
+
 # nothing happens until we call the methods
+students = input_students
 print_header
 print(students)
 print_footer(students)
