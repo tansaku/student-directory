2d0789a47cdcf476587ba54ea65c0b7a29749a97
Sam Joseph <tansaku@gmail.com>
2014-01-12 10:28:27 +0000

    adding ability to save students

--- directory.rb
+++ directory.rb
@@ -64,7 +64,6 @@ def input_students
     # get another name from the user
     student = gets.delete("\n")
   end
-
 end
 
 MONTHS = ['january','february','march','april','may','june','july','august','september','october','november','december']
@@ -100,6 +99,7 @@ end
 def print_menu 
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit" # 9 because we'll be adding more items
 end
 
@@ -107,6 +107,18 @@ def show_students
   print_header
   print_students_list
   print_footer
+end
+
+def save_students
+  # open the file for writing
+  file = File.open("students.csv", "w")
+  # iterate over the array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
 end
 
 def interactive_menu
@@ -122,6 +134,8 @@ def process(selection)
     input_students
   when "2"
     show_students
+  when "3"
+    save_students
   when "9"
     exit
   else
