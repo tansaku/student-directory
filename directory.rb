82ed57f8d5dca351d864cc3e8053d50997fb76b0
Sam Joseph <tansaku@gmail.com>
2014-01-12 10:05:56 +0000

    student directory pro interactive menu set up

--- directory.rb
+++ directory.rb
@@ -99,15 +99,34 @@ end
 
 
 def interactive_menu
+  students = []
+  loop do
   # 1. print the menu and ask the user what to do
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit" # 9 because we'll be adding more items
   # 2. read the input and save it into a variable
+  selection = gets.chomp
   # 3. do what the user has asked
-  # 4. repeat from step 1
+  case selection
+  when "1"
+    # input the students
+  when "2"
+    print_header
+    print(students)
+    print_footer(students)
+  when "9"
+    exit # this will cause the program to terminate
+  else
+    puts "I don't know what you meant, try again"
+  end
+  end
 end
 
 
 # nothing happens until we call the methods
 #students = input_students
-print_header
-print(students)
-print_footer(students)
+# print_header
+# print(students)
+# print_footer(students)
+interactive_menu
