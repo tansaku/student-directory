2890c0c04f2909881d2e11b6e8419941eefc4e37
Sam Joseph <tansaku@gmail.com>
2014-01-16 09:13:29 +0000

    using file blocks

--- directory.rb
+++ directory.rb
@@ -100,23 +100,23 @@ end
 
 def save_students
   # open the file for writing
-  file = File.open("students.csv", "w")
-  # iterate over the array of students
-  @students.each do |student|
-    student_data = [student[:name], student[:cohort]]
-    csv_line = student_data.join(",")
-    file.puts csv_line
+  File.open("students.csv", "w") do |file|
+    # iterate over the array of students
+    @students.each do |student|
+      student_data = [student[:name], student[:cohort]]
+      csv_line = student_data.join(",")
+      file.puts csv_line
+    end
   end
-  file.close
 end
 
 def load_students(filename = "students.csv")
-  file = File.open(filename, "r")
-  file.readlines.each do |line|
-    name, cohort = line.chomp.split(',')
-    add_student(name, cohort)
+  File.open(filename, "r") do |file|
+	  file.readlines.each do |line|
+	    name, cohort = line.chomp.split(',')
+	    add_student(name, cohort)
+	  end
   end
-  file.close
 end
 
 def try_load_students
