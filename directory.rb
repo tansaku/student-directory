cb418fa3c1b19033bb6fd0b320c3616cfe692b5f
Sam Joseph <tansaku@gmail.com>
2014-01-22 09:08:08 +0000

    using print with newline instead of puts - watch out for collision with existing print method

--- directory.rb
+++ directory.rb
@@ -12,24 +12,24 @@ students = [
 ]
 
 def print_header
-  puts "The students of my cohort at Makers Academy"
-  puts "-------------"
+  print "The students of my cohort at Makers Academy\n"
+  print "-------------\n"
 end
 
-def print(students)
+def print_students(students)
   students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+    print "#{student[:name]} (#{student[:cohort]} cohort)\n"
   end
 end
 
 
 def print_footer(names)
-  puts "Overall, we have #{names.length} great students"
+  print "Overall, we have #{names.length} great students\n"
 end
 
 def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
+  print "Please enter the names of the students\n"
+  print "To finish, just hit return twice\n"
   # create an empty array
   students = []
   # get the first name
@@ -38,7 +38,7 @@ def input_students
   while !name.empty? do
     # add the student hash to the array
     students << {:name => name, :cohort => :november}
-    puts "Now we have #{students.length} students"
+    print "Now we have #{students.length} students\n"
     # get another name from the user
     name = gets.chomp
   end
@@ -49,5 +49,5 @@ end
 # nothing happens until we call the methods
 students = input_students
 print_header
-print(students)
+print_students(students)
 print_footer(students)
